fb8b4bd000e5e761aed285ebe8dc832bd3a809a9
appleninja360 <30459984+appleninja360@users.noreply.github.com>
2021-02-22 22:52:25 -0500

    Add project files.

---
+++ Youtube game/Youtube game.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void Menu()
+{
+	int choice;
+	do 
+	{
+		cout << "0. Play\n1. Settings\n2. creits\n3. exit\n\n";
+		cin >> choice;
+		switch (choice)
+		{
+		case 0:
+			cout << "You are playing the game\n";
+			break;
+		case 1:
+			cout << "Your in the settings\n";
+			break;
+		case 2:
+			cout << "by appleninja360YT \n";
+			break;
+		case 3:
+			cout << "Ok Bye\n";
+			break;
+		}
+
+
+	} while (choice != 3);
+	
+}
+
+int main()
+{
+	Menu();
+}
